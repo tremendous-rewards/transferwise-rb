d3c3ef3fa52a13b4520a94b9ce0593d6a4fc2a2a
Arianit Gerqina <arianitgerqina@gmail.com>
2018-06-04 09:20:03 +0200

    add transfer requirements and cancel transfer methods

--- lib/transfer_wise/transfer.rb
+++ lib/transfer_wise/transfer.rb
@@ -4,5 +4,15 @@ module TransferWise
       response = TransferWise::Request.request(:post, "#{resource_url(resource_id)}/payments", params, opts)
       convert_to_transfer_wise_object(response)
     end
+
+    def self.transfer_requirements(params, opts = {})
+      response = TransferWise::Request.request(:post,  "/#{API_VERSION}/transfer-requirements", params,  opts)
+      convert_to_transfer_wise_object(response)
+    end
+
+    def self.cancel(resource_id,  opts = {})
+      response = TransferWise::Request.request(:put, "#{resource_url(resource_id)}/cancel", {},  opts)
+      convert_to_transfer_wise_object(response)
+    end
   end
 end
